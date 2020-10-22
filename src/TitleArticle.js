a05209e319dadf95d81a754189497cb0266bfce7
ackerman <do.crazy192@gmail.com>
2020-10-22 14:06:28 +0700

    Create Component TitleArticle

---
+++ src/TitleArticle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TitleArticle = ({ text }) => {
+    return (
+        <div>
+            <h1> {text}</h1>
+        </div>
+    )
+}
+
+export default TitleArticle
