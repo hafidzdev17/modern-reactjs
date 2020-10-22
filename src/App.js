a30fec799794398ad6cc208d0745fea52d9b2d08
ackerman <do.crazy192@gmail.com>
2020-10-22 15:26:25 +0700

    last commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello'
 import Article from './Article'
+import Rendering from './Rendering'
+import Conditional from './Conditional'
+import List from './ListRendering'
 
 function App () {
   return (
     <div className="App">
+      <hr />
+      <h1>Parent & Child Component</h1>
       <Article userLogin='sudah login' />
+      <hr />
+      <h1>Conditional Rendering</h1>
+      <Conditional username="hafidz" />
+      <hr />
+      <h1>List Rendering</h1>
+      <List />
+      {/* <Rendering /> */}
     </div>
   );
 }
