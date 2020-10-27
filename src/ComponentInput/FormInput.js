214d0f696d09d621a363a03fa831c9b78a0a92f5
ackerman <do.crazy192@gmail.com>
2020-10-27 20:26:43 +0700

    create FormInput

---
+++ src/ComponentInput/FormInput.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import InputText from './InputText'
+import TextArea from './TextArea'
+import Select from './Select'
+
+const FormInput = () => {
+    // const [name, setName] = React.useState('')
+    // const [alamat, setAlamat] = React.useState('')
+    // const [jurusan, setJurusan] = React.useState('')
+    // teknik cepat
+    const [form, setForm] = React.useState({
+        name: '',
+        alamat: '',
+        jurusan: ''
+    })
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        // sent to server
+        alert(
+            JSON.stringify((form))
+        )
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <InputText label='name' value={form.name} onChange={(e) =>
+                    setForm({ ...form, name: e.target.value })} />
+                <TextArea label="alamat" value={form.alamat} onChange={(e) =>
+                    setForm({ ...form, alamat: e.target.value })} />
+                <Select label="jurusan" value={form.jurusan} onChange={(e) =>
+                    setForm({ ...form, jurusan: e.target.value })} />
+                <input type="submit" value="Submit" />
+            </form>
+
+        </div>
+    )
+}
+
+export default FormInput
