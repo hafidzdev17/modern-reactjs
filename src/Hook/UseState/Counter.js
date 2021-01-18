f607c4c7e121778e3e0d07c4ff1023c163cfd097
Hafet17 <do.crazy192@gmail.com>
2021-01-18 13:31:12 +0700

    useState

---
+++ src/Hook/UseState/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Counter = ({ text }) => {
+    const [counter, setCounter] = useState(0)
+
+    const handleAdd = () => setCounter(counter + 1)
+    const handleMinus = () => setCounter(counter - 1)
+
+    if (counter < 0) {
+        console.log('oppsssssssss');
+        setCounter(counter + 1)
+    }
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>{text}</h1>
+            <button style={{ backgroundColor: 'limegreen', color: 'white' }} onClick={handleAdd}>+</button>
+            <h1 style={{ color: 'red' }}> {counter} </h1>
+            <button style={{ backgroundColor: 'blue', color: 'white' }} onClick={handleMinus}>-</button>
+        </div>
+    )
+}
+
+export default Counter
